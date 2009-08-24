810e75d81b62511285645c3bcd210328ffd55eaf
Jerad Bitner <sirkitree@gmail.com>
2009-08-24 22:41:28 +0000

    making sure that the variable we need is defined before operation upon it.

--- smackdown.js
+++ smackdown.js
@@ -19,17 +19,19 @@
    *   The jQuery object to apply the behaviors to.
    */
   Drupal.behaviors.smackdown = function(context) {
-    var field_1 = '.field-' + Drupal.settings.smackdown.field1.name.replace(/_/g, '-');
-    var field_2 = '.field-' + Drupal.settings.smackdown.field2.name.replace(/_/g, '-');
-    if (Drupal.settings.smackdownPerm == 1) {
-      Drupal.smackdown.attachVote(context, '.' + field_1 + ' .field-item a');
-      $(field_1 + ' .field-item a').attr('rel', Drupal.settings.smackdown.field1.nid).addClass('smackdown-processed');
-      Drupal.smackdown.attachVote(context, '.' + field_2 + ' .field-item a');
-      $(field_2 + ' .field-item a').attr('rel', Drupal.settings.smackdown.field2.nid).addClass('smackdown-processed');
-    }
-    else {
-      Drupal.smackdown.attachNotice(context, '.' + field_1 + ' .field-item a');
-      Drupal.smackdown.attachNotice(context, '.' + field_2 + ' .field-item a');
+    if (typeof Drupal.settings.smackdown !== 'undefined') {
+      var field_1 = '.field-' + Drupal.settings.smackdown.field1.name.replace(/_/g, '-');
+      var field_2 = '.field-' + Drupal.settings.smackdown.field2.name.replace(/_/g, '-');
+      if (Drupal.settings.smackdownPerm == 1) {
+        Drupal.smackdown.attachVote(context, '.' + field_1 + ' .field-item a');
+        $(field_1 + ' .field-item a').attr('rel', Drupal.settings.smackdown.field1.nid).addClass('smackdown-processed');
+        Drupal.smackdown.attachVote(context, '.' + field_2 + ' .field-item a');
+        $(field_2 + ' .field-item a').attr('rel', Drupal.settings.smackdown.field2.nid).addClass('smackdown-processed');
+      }
+      else {
+        Drupal.smackdown.attachNotice(context, '.' + field_1 + ' .field-item a');
+        Drupal.smackdown.attachNotice(context, '.' + field_2 + ' .field-item a');
+      }
     }
   };
 
